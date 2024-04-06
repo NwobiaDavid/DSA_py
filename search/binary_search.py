0a052250dde4a244f518858df4ce6e6426b4fc87
NwobiaDavid <dnwobia@gmail.com>
2024-04-06 21:23:57 +0100

    updated the binary search algorithm

--- search/binary_search.py
+++ search/binary_search.py
@@ -1,36 +1,36 @@
-
 def test_location(arrayy, query, midpoint):
     if arrayy[midpoint] == query:
-        if midpoint - 1 > 0 and arrayy[midpoint - 1] == query:
-            return "left"
-        else:
+        if midpoint == 0 or arrayy[midpoint - 1] != query:
             return "found"
+        else:
+            return "left"
     elif arrayy[midpoint] < query:
-        return "left"
-    else:
         return "right"
+    else:
+        return "left"
 
 
 def binary_search(arrayy, query):
     first = 0
-    last = len(arrayy)-1
+    last = len(arrayy) - 1
 
     while first <= last:
-        midpoint = (first+last) // 2
+        midpoint = (first + last) // 2
 
         result = test_location(arrayy, query, midpoint)
 
         if result == "found":
-            print("value you are searching for is at index: ", midpoint)
+            print("Value you are searching for is at index:", midpoint)
             return midpoint
         elif result == "left":
-            first = midpoint + 1
-        else:
             last = midpoint - 1
-    print("element doesn't exist in the array")
+        else:
+            first = midpoint + 1
+
+    print("Element doesn't exist in the array")
     return None
 
 
+# Example usage:
 numbers = [1, 1, 22, 90, 232, 232, 499]
-binary_search(numbers, 90)
-
+binary_search(numbers, 22)
